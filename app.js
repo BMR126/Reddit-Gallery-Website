d537dbe39f01389d55292f02e1fae5406bfbe561
Brandon Romito <83603118+BMR126@users.noreply.github.com>
2025-07-14 14:01:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static('Public'));
 
 const reddit = new Snoowrap({
   userAgent: process.env.REDDIT_USER_AGENT,
